e5f6a9990fa7a9b0755223a15ae3cc6da98cb006
andrtw <andrea96.t@gmail.com>
2022-06-27 12:33:16 +0200

    Updated dependencies

--- app/src/main/java/com/andrtw/nycfarmersmarkets/MainActivity.kt
+++ app/src/main/java/com/andrtw/nycfarmersmarkets/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.andrtw.nycfarmersmarkets.ui.theme.NycfarmersmarketsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NycfarmersmarketsTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Text("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NycfarmersmarketsTheme {
-        Greeting("Android")
     }
 }
